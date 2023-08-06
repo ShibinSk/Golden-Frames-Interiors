e9b33cc412781381801d5b73881b7aaa94983ea9
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2023-08-06 13:06:57 +0530

    nn

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ mongoose.connect(process.env.MONGO_DB ?? 'mongodb://127.0.0.1:27017/GFI').then((
 })
   .catch(console.log);
 
+  mongoose.set('strictQuery', false);
   const PORT = (process.env.PORT || '3000');
   app.listen(PORT,()=>{
     console.log(" Connected " + PORT)
